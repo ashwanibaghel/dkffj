d2c79ab3147768331c8c373e6e017f1451a0af97
Ashwani Baghel <ashwanibaghel826@gmail.com>
2026-07-09 02:33:31 +0530

    feat(footer): add Facebook and X Twitter links from old PHP site DB - facebook.com/dkffjorg + twitter.com/dkfofaj

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1400,6 +1400,30 @@ export default function Home() {
                   <path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z"/>
                 </svg>
               </a>
+              {/* Facebook */}
+              <a
+                href="https://www.facebook.com/dkffjorg?mibextid=ZbWKwL"
+                target="_blank"
+                rel="noopener noreferrer"
+                title="Facebook Page"
+                className="w-7 h-7 rounded-full bg-slate-200 hover:bg-[#1877F2] hover:text-white flex items-center justify-center transition-all text-slate-600"
+              >
+                <svg className="w-4 h-4 fill-current" viewBox="0 0 24 24">
+                  <path d="M24 12.073c0-6.627-5.373-12-12-12s-12 5.373-12 12c0 5.99 4.388 10.954 10.125 11.854v-8.385H7.078v-3.47h3.047V9.43c0-3.007 1.792-4.669 4.533-4.669 1.312 0 2.686.235 2.686.235v2.953H15.83c-1.491 0-1.956.925-1.956 1.874v2.25h3.328l-.532 3.47h-2.796v8.385C19.612 23.027 24 18.062 24 12.073z"/>
+                </svg>
+              </a>
+              {/* X (Twitter) */}
+              <a
+                href="https://twitter.com/dkfofaj?t=grzhZFyCWEFKUlR0nnW-6g&s=08"
+                target="_blank"
+                rel="noopener noreferrer"
+                title="X (Twitter)"
+                className="w-7 h-7 rounded-full bg-slate-200 hover:bg-black hover:text-white flex items-center justify-center transition-all text-slate-600"
+              >
+                <svg className="w-3.5 h-3.5 fill-current" viewBox="0 0 24 24">
+                  <path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-4.714-6.231-5.401 6.231H2.74l7.73-8.835L1.254 2.25H8.08l4.259 5.631 5.905-5.631zm-1.161 17.52h1.833L7.084 4.126H5.117z"/>
+                </svg>
+              </a>
             </div>
             <span className="text-[10px] text-slate-400 mt-2 block">
               &copy; {new Date().getFullYear()} DKFFJ. All rights reserved.
